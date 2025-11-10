fe722d7f67aa013e10fe13e9daedad464c060aa9
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-11-11 01:41:41 +0600

    try to fix the err

--- src/components/aboutPage/AboutHome.jsx
+++ src/components/aboutPage/AboutHome.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import Navbar from '../shared/navbar/Navbar';
-import AboutBanner from './AboutBanner';
-import AboutArea from './AboutArea';
-import Team from './Team';
-import Footer from '../shared/footer/Footer';
-import AboutTeam from './AboutTeam';
+import React from "react";
+import dynamic from "next/dynamic";
+import Navbar from "../shared/navbar/Navbar";
+import AboutBanner from "./AboutBanner";
+import AboutArea from "./AboutArea";
+import Team from "./Team";
+import Footer from "../shared/footer/Footer";
 
+// ✅ Disable SSR for AboutTeam so it only loads in the browser
+const AboutTeam = dynamic(() => import("./AboutTeam"), {
+  ssr: false,
+  loading: () => <div className="min-h-[50vh] bg-black" />, // optional fallback
+});
 
 const AboutHome = () => {
-    return (
-        <div>
-            <div className=''>
-                <Navbar />
-            </div>
-            <AboutBanner />
-            <AboutArea />
-            <Team />
-            <AboutTeam />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <AboutBanner />
+      <AboutArea />
+      <Team />
+      <AboutTeam />
+      <Footer />
+    </div>
+  );
 };
 
 export default AboutHome;
