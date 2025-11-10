a8a6d6de9155aaa382fb6471b3793d4851b403cf
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-11-11 01:49:29 +0600

    remove the about team

--- src/components/aboutPage/AboutHome.jsx
+++ src/components/aboutPage/AboutHome.jsx
@@ -1,17 +1,11 @@
-'use client'
 import React from "react";
-import dynamic from "next/dynamic";
 import Navbar from "../shared/navbar/Navbar";
 import AboutBanner from "./AboutBanner";
 import AboutArea from "./AboutArea";
 import Team from "./Team";
 import Footer from "../shared/footer/Footer";
 
-// ✅ Disable SSR for AboutTeam so it only loads in the browser
-const AboutTeam = dynamic(() => import("./AboutTeam"), {
-  ssr: false,
-  loading: () => <div className="min-h-[50vh] bg-black" />, // optional fallback
-});
+
 
 const AboutHome = () => {
   return (
@@ -20,7 +14,7 @@ const AboutHome = () => {
       <AboutBanner />
       <AboutArea />
       <Team />
-      <AboutTeam />
+
       <Footer />
     </div>
   );
